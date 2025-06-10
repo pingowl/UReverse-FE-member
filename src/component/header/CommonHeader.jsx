fc87739a635907df3e4c4481747744e1f1e821f9
pch8349 <chan449@naver.com>
2025-06-10 17:49:05 +0900

    feat: 헤더 아이콘 추가

--- src/component/header/CommonHeader.jsx
+++ src/component/header/CommonHeader.jsx
@@ -1,12 +1,15 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom";
+import styles from "./CommonHeader.module.css";
+import notificationIcon from "../../assets/icon-notification-bell.png"
+import userIcon from "../../assets/icon-user.png"
 
-export default function CommonHeader () {
+export default function NoAlarmHeader () {
     const location = useLocation();
     const navigate = useNavigate();
     // 경로에 따른 페이지 이름 매핑
     const titleMap = {
         '/mypage': '마이페이지',
-        '/sell': '판매'
+        '/sell': '판매하기'
     };
 
     // location.pathname이 포함된 key를 찾는 로직
@@ -17,21 +20,28 @@ export default function CommonHeader () {
     const title = titleMap[matchedKey] || '페이지';
 
     return (
-        <header className="w-full flex items-center justify-between py-4 px-4">
+        <header className={styles.header}>
            {/* 왼쪽: 뒤로가기 버튼 */}
             <button
                 onClick={() => navigate(-1)}
-                className="text-xl text-gray-600 hover:text-blue-500"
+                className={styles.backButton}
                 aria-label="뒤로가기"
             >
                 &lt;
             </button>
 
             {/* 가운데: 페이지 제목 */}
-            <h1 className="text-lg font-semibold text-gray-800">{title}</h1>
+            <h1 className={styles.title}>{title}</h1>
 
-            {/* 오른쪽: 공간 유지를 위해 빈 div */}
-            <div className="w-6" /> {/* 아이콘 공간 대체용 */}
+            {/* 오른쪽: 아이콘 영역 */}
+            <div className={styles.iconGroup}>
+                <button className={styles.iconButton} aria-label="알림">
+                    <img src={notificationIcon} alt="alarm" className={styles.image} />
+                </button>
+                <button className={styles.iconButton} aria-label="마이페이지">
+                    <img src={userIcon} alt="myPage" className={styles.image} />
+                </button>
+            </div>
         </header>
     )
 }
