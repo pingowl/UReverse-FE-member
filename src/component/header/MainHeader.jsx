7640b61d73dae7262ceefc7808f1a021776e5f7b
pch8349 <chan449@naer.com>
2025-06-18 00:57:12 +0900

    feat: SSE 통신 헤더 알림 아이콘 연결, accessToken 만료시 자동 재발급

--- src/component/header/MainHeader.jsx
+++ src/component/header/MainHeader.jsx
@@ -16,9 +16,6 @@ export default function MainHeader () {
             {/* 오른쪽: 아이콘 영역 */}
             <div className={styles.iconGroup}>
                 <NotificationIcon/>
-                {/* <button className={styles.iconButton} aria-label="알림" onClick={() => navigate('/notifications')}>
-                    <img src={notificationIcon} alt="alarm" className={styles.icon} />
-                </button> */}
                 <button className={styles.iconButton} aria-label="마이페이지" onClick={() => navigate('/mypage')}>
                     <img src={userIcon} alt="myPage" className={styles.icon} />
                 </button>
--- src/component/icon/NotificationIcon.jsx
+++ src/component/icon/NotificationIcon.jsx
@@ -3,15 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import notificationIconImg from '../../assets/icon-notification-bell.png';
 import styles from './NotificationIcon.module.css';
-import axios from 'axios';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { authState } from '../../atoms/authState';
 import api from '../../api/axiosInstance';
+import { userState } from '../../atoms/userState';
+import { connectSSE } from '../../util/ConnectSSE';
 
 export default function NotificationIcon() {
     const navigate = useNavigate();
     const [unreadCount, setUnreadCount] = useState(0);
-    const { accessToken } = useRecoilValue(authState);
+    const { accessToken, role } = useRecoilValue(authState);
+    const setAuth = useSetRecoilState(authState);
+    const setUser = useSetRecoilState(userState);
 
     useEffect(() => {
         // 초기 한 번만 불러오기
@@ -23,7 +26,45 @@ export default function NotificationIcon() {
                 console.error('알림 수 조회 실패:', e);
             }
         };
+
+        let eventSource;
+        let reconnectTimer;
+        const setupSSE = async () => {
+            try {
+                const userId = JSON.parse(atob(accessToken.split('.')[1])).sub;
+                eventSource = connectSSE(accessToken, userId, fetchUnreadCount);
+
+                eventSource.onerror = async () => {
+                    eventSource.close();
+
+                    try {
+                        // accessToken 재발급 시도
+                        const res = await api.get('/api/v1/auth/refresh');
+                        const { accessToken: newAccessToken, role } = res.data.response;
+
+                        setAuth({ accessToken: newAccessToken, role });
+
+                        reconnectTimer = setTimeout(() => {
+                            setupSSE(); // 재연결
+                        }, 1000); // 짧은 지연후 연결 재시도
+                    } catch (err) {
+                        console.error('refreshToken 만료, 로그인 페이지로 이동');
+                        setAuth({ accessToken: null, role: null });
+                        setUser({ userId: null, name: '', email: '', phone: '', role: '', isLoggedIn: false });
+                        navigate('/login');
+                    }
+                }
+            } catch (err) {
+                console.error('SSE 설정 중 에러 : ', err);
+            }
+        }
         fetchUnreadCount();
+        setupSSE();
+
+        return () => {
+            if (eventSource) eventSource.close();
+            if (reconnectTimer) clearTimeout(reconnectTimer);
+        }
     }, []);
 
     return (
